6be70fe238cc5ed7b6b409c5da9086ca581255b9
myname <erem151999@mail.ru>
2022-03-04 11:44:12 +0300

    adaptive advatages

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,10 @@
 
+document.addEventListener('click', function (e) {
+    let targetEl = e.target;
+
+    if (targetEl.classList.contains('header__burger')) {
+        targetEl.classList.toggle('_active');
+        document.querySelector('.header__menu').classList.toggle('_active');
+        document.querySelector('.page').classList.toggle('_moved');
+    }
+});
