bf03a83d02a98927257958ad7a3440a123d9c2cc
myname <erem151999@mail.ru>
2022-03-04 18:47:39 +0300

    finished layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,22 @@
-
 document.addEventListener('click', function (e) {
     let targetEl = e.target;
-
     if (targetEl.classList.contains('header__burger')) {
         targetEl.classList.toggle('_active');
         document.querySelector('.header__menu').classList.toggle('_active');
         document.querySelector('.page').classList.toggle('_moved');
     }
 });
+
+if (document.querySelector('.feedback__slider')) {
+    new Swiper('.feedback__slider', {
+        slidesPerView: 'auto',
+        loop: true,
+        spaceBetween: 21,
+        grabCursor: true,
+        pagination: {
+            el: '.feedback__slider-pagination',
+            clickable: true,
+        },
+
+    })
+}
