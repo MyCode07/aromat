48c03c4ad55a1518e57cea2be5ed8e43836ef463
myname <erem151999@mail.ru>
2022-03-07 14:36:56 +0300

    finished

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,25 @@ document.addEventListener('click', function (e) {
         document.querySelector('.header__menu').classList.toggle('_active');
         document.querySelector('.page').classList.toggle('_moved');
     }
+
+    let arr = { block: "start", behavior: "smooth" };
+
+    if (targetEl.classList.contains('scroll-to-advantages')) {
+        prevDef(e);
+        document.querySelector('.advantages').scrollIntoView(arr);
+    }
+    if (targetEl.classList.contains('scroll-to-about')) {
+        prevDef(e);
+        document.querySelector('.about').scrollIntoView(arr);
+    }
+    if (targetEl.classList.contains('scroll-to-feedback')) {
+        prevDef(e);
+        document.querySelector('.feedback').scrollIntoView(arr);
+    }
+    function prevDef(e) {
+        e.preventDefault();
+    }
+
 });
 
 if (document.querySelector('.feedback__slider')) {
